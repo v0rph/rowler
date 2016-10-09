c74311c928675b2c0bcd89f8a693431bc300fe92
Hugo Marinho <hugompmarinho@gmail.com>
2016-10-09 19:15:27 +0200

    added controller tests

---
+++ test/controllers/api/v1/games_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+module Api::V1
+  class GamesControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @game = FactoryGirl.create(:game)
+    end
+
+    should "create game" do
+      expect {
+        post v1_games_url
+      }.must_change "Game.count"
+
+      value(response.status).must_equal 201
+    end
+
+    should "show game" do
+      get v1_game_url(id: @game.id)
+      value(response).must_be :success?
+    end
+
+    should "return 404 if game does not exist" do
+      get v1_game_url(id: -1)
+      value(response).must_be :missing
+    end
+
+    should "update game" do
+      patch v1_game_url(id: @game.id), params: { throw: 0 }
+      value(response.status).must_equal 200
+    end
+  end
+end
---
+++ test/factories/game_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :game do
+    current_frame  1
+    finished       false
+  end
+end
